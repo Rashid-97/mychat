90ad66c7ed0bf1e8ca2702076b0e4d21bba5fdd8
Rashid-97 <rashidm9797@gmail.com>
2024-08-21 15:14:41 +0400

    debug true

--- mychat/settings.py
+++ mychat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6)t1%($$t5s3jl)0bzthmbd@1e1#r-l9j80goq(90$gty)%*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
